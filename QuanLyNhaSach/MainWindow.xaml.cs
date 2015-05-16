c86528c6914e9a3575fe767158d99135813a8eed
Thu Thôi <camthu.vuive@gmail.com>
2015-05-17 00:01:46 +0700

    Giao dien tabControl

---
+++ QuanLyNhaSach/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Ribbon;
+
+namespace QuanLyNhaSach
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : DXRibbonWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            bNgayThang.Content = DateTime.Now.ToShortDateString();
+        }
+
+        private void btnQLNhanVien_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+            //DXTabItem newTab = new DXTabItem();
+            //newTab.Header = "Quản lý nhân viên";
+            //QuanLyNhanVien qlnv = new QuanLyNhanVien();
+            //newTab.Content = qlnv;
+            //tabControl.Items.Add(newTab);
+        }
+    }
+}
